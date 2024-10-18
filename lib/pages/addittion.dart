5703db970a63574a01ef41d98d35d0a9f2a1a3cf
Snehatk2002 <sammu5285@gmail.com>
2024-10-18 19:32:03 +0530

    created flutter app

---
+++ lib/pages/addittion.dart
@@ -0,0 +1,108 @@
+import 'package:calculate/pages/add.dart';
+import 'package:calculate/pages/div.dart';
+import 'package:calculate/pages/mul.dart';
+import 'package:calculate/pages/sub.dart';
+import 'package:flutter/material.dart';
+
+class Addition extends StatelessWidget {
+  const Addition({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SizedBox(height: 20,),
+          SizedBox(
+            height: 50,
+            width: 400,
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.pink,
+              ),
+              onPressed: (){
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context)=>Add() ));
+              },
+              child: Text(
+                "ADDITION",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold
+                ),
+              ),
+            ),
+
+          ),
+          SizedBox(height: 20,),
+          SizedBox(
+            height: 50,
+            width: 400,
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.pink,
+              ),
+              onPressed: (){
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context)=>Sub() ));
+              }, child: Text(
+              "SUBTRACTION",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold
+              ),
+            ),
+            ),
+          ),
+          SizedBox(height: 20,),
+          SizedBox(
+            height: 50,
+            width: 400,
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.pink,
+              ),
+              onPressed: (){
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context)=>Mul() ));
+              }, child: Text(
+              "MULTIPLICATION",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold
+              ),
+            ),
+            ),
+          ),
+          SizedBox(height: 20,),
+          SizedBox(
+            height: 50,
+            width: 400,
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.pink,
+              ),
+              onPressed: (){
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context)=>Div() ));
+              },
+              child: Text(
+                "DIVISION",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold
+                ),
+              ),
+
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
